3f3d2b7ecf3d3f7c7acb37240dc3ba6d6cf8b4ce
mikedotexe <mikedotexe@gmail.com>
2013-09-17 18:05:20 -1000

    testing curl on my site

--- test.php
+++ test.php
@@ -4,6 +4,23 @@
 	} else {
 	echo 'does not exist';
 	}
+	
+$service_url = 'http://www.mikedotexe.com/index.php/app/plants';
+$curl = curl_init($service_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($curl);
+$decoded = json_decode($curl_response);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('error occured: ' . $decoded->response->errormessage);
+}
+echo 'response ok!<br/><br/>';
+var_dump($decoded);	
     /**
      * Some very basic php that demonstrates how to make an access token request
      * on behalf of a LearningStudio user via the OAuth 2.0 Password grant type
